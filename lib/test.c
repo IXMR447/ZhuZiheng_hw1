8a58ecbc9b690cd886467016967bc84f1ccc6267
IXMR447 <04075257zzh@gmail.com>
2025-04-26 10:19:31 +0800

    second save

--- lib/test.c
+++ lib/test.c
@@ -3,5 +3,11 @@
 int main()
 {
     printf("Hello World\n");
+    if(1) {
+        printf("This is a test\n");
+    } else {
+        printf("This is not a test\n");
+    }
+    printf("This is a test\n");
     return 0;
 }
